31ce5cda4f3e6f5cf3aee2f46ac3bd89264d1ef7
Ray Badua <rayjonathanbadua@gmail.com>
2023-07-05 11:37:34 -0700

    starter code, schema.sql has db name

---
+++ controllers/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+
+const apiRoutes = require('./api')
+
+const viewRoutes = require('./viewRoutes')
+
+router.use('/', viewRoutes);
+router.use('/api', apiRoutes);
+
+module.exports = router;
---
+++ db/schema.sql
@@ -0,0 +1,4 @@
+DROP DATABASE IF EXISTS balldontlie_db;
+
+CREATE DATABASE balldontlie_db;
+
