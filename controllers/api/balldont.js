013d8790d5935ddf64bad43dbbb22404c0002bd1
epicasino2 <rayjonathanbadua@gmail.com>
2023-07-06 16:22:39 -0700

    app.js, balldont.js, if user isn't logged in, user is redirected to login page

--- controllers/api/balldont.js
+++ controllers/api/balldont.js
@@ -4,18 +4,20 @@ const { Player } = require("../../models");
 
 // Gets player data on balldontlie
 router.post("/save", async (req, res) => {
-  try {
-    const newPlayer = await Player.create({
-      player_id: req.body.id,
-      first_name: req.body.first_name,
-      last_name: req.body.last_name,
-      team_name: req.body.team_name,
-      user_id: req.session.userId,
-    });
+  if (req.session.loggedIn) {
+    try {
+      const newPlayer = await Player.create({
+        player_id: req.body.id,
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        team_name: req.body.team_name,
+        user_id: req.session.userId,
+      });
 
-    res.status(200).json({ message: "Player successfully saved!" });
-  } catch (error) {
-    res.status(500).json(error);
-  }
+      res.status(200).json({ message: "Player successfully saved!" });
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else res.status(400).json({ message: 'Not logged in'});
 });
 module.exports = router;
--- public/js/app.js
+++ public/js/app.js
@@ -7,12 +7,15 @@ testBtn.addEventListener("click", async () => {
     last_name: "James",
     team_name: "Los Angeles Lakers",
   };
-
   const response = await fetch("/api/balldontlie/save", {
     method: "POST",
     body: JSON.stringify(addedPlayer),
     headers: { "Content-Type": "application/json" },
   });
+
+  if (!response.ok) {
+    window.location.replace("/login");
+  }
 
   console.log(response);
 });
