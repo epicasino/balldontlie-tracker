a69a016064580f9bd51fb50137f5868b2071c5a3
Ray Badua <rayjonathanbadua@gmail.com>
2023-07-12 13:11:48 -0700

    navbar styling

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,35 +1,34 @@
 const loginStatusEl = document.getElementById("loginStatusBtn");
 const logoutBtn = document.getElementById("logoutBtn");
 const authLogin = async () => {
-  const loginStatus = await axios.post("/api/user/auth");
-  console.log(loginStatus);
-  if (loginStatus.status === 404) {
-    logoutBtn.textContent = "";
-  }
+  const loginStatus = await fetch("/api/user/auth", {
+    method: "POST",
+  });
 
   if (loginStatus.status === 200) {
-    loginStatusEl.textContent = loginStatus.data;
+    const loginUsername = await loginStatus.json();
+    console.log(loginStatus);
+
+    loginStatusEl.textContent = loginUsername;
     loginStatusEl.setAttribute("href", "/dashboard");
+    loginStatusEl.setAttribute("class", "nav-link border border-warning mx-3");
+
+    logoutBtn.setAttribute("style", "display: relative;");
+    logoutBtn.setAttribute("class", "nav-link border border-warning");
     logoutBtn.textContent = "Logout";
+  } else if (loginStatus.status === 404) {
+    loginStatusEl.textContent = "Login";
+    loginStatusEl.setAttribute("href", "/dashboard");
+    loginStatusEl.setAttribute("class", "nav-link border border-warning");
+    logoutBtn.setAttribute("style", "display:none");
   }
 };
 
-// const check = () => {
-//     if (loginStatusEl.textContent !== 'Login') {
-//         authLogin();
-//     } else {
-//     }
-// };
-
-// check();
 authLogin();
 
 const authLogout = async () => {
   const logoutStatus = await axios.post("/api/user/logout");
   if (logoutStatus.status === 200) {
-    logoutBtn.setAttribute("href", "/");
-    logoutBtn.textContent = "";
-    loginStatusEl.textContent = "Login";
     alert("Logged out!");
     window.location.replace("/");
   }
